cfb281f64234bc2387d4f2224d90291908892675
lerokmelnichenko <lerokmelnichenko@gmail.com>
2025-05-23 11:00:04 +0300

    Inti
    Init

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    """Повертає суму двох чисел."""
+    return a + b
+
+def multiply(a, b):
+    """Повертає добуток двох чисел."""
+    return a * b
+
+if __name__ == "__main__":
+    x = 5
+    y = 7
+    print(f"Sum of {x} and {y} is {add(x, y)}")
+    print(f"Product of {x} and {y} is {multiply(x, y)}")
